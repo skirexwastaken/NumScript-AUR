e0f89074fb5d7b19f6e6af42a2b4880cf94e00b3
Daniel <hora.daniel@protonmail.com>
2025-12-12 22:29:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="numscript",
     version="1.0.0",
-    packages=find_packages(),  # will now find NumScript/ and source/
+    packages=find_packages(include=["NumScript", "NumScript.*"]),
     install_requires=[],
     entry_points={
         "console_scripts": [
