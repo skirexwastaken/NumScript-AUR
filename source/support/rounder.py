e3c8a19e1dbd5a58e9b906ef8b6aff38656071b1
Daniel <hora.daniel@protonmail.com>
2025-12-12 22:09:11 +0100

    Delete source directory

--- source/support/rounder.py
+++
@@ -1,6 +0,0 @@
-# --- Makes sure proper token format is kept at output ---
-def rounder(self, tokensToRound):
-    if len(tokensToRound) % 2 == 1:
-        return"0"+tokensToRound
-    
-    return(tokensToRound)
--- source/support/tokenCorrector.py
+++
@@ -1,9 +0,0 @@
-# --- Corrects wrong tokens  ---
-def tokenCorrector(self):
-    if self.higherTokenizedCode:
-        line = self.higherTokenizedCode[self.higherLindex]
-        self.higherTokenizedCode[self.higherLindex] = line[:-len(line) + self.depth + 1]
-
-    else:
-        line = self.tokenizedCode[self.lindex]
-        self.tokenizedCode[self.lindex] = line[:-len(line) + self.depth + 1]
