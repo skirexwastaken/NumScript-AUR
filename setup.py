c2994da0a1c04051996010fd07b7e269dc01eb07
Daniel <hora.daniel@protonmail.com>
2025-12-12 23:08:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,22 @@ setup(
     name="numscript",
     version="1.0.0",
     packages=find_packages(include=["NumScript", "NumScript.*"]),
-    install_requires=[],
+    include_package_data=True,
+    package_data={
+        "NumScript": [
+            "source/json/*.json",
+            "source/*.ns", 
+        ]
+    },
     entry_points={
         "console_scripts": [
             "numscript=NumScript.NumScript:main",
         ],
     },
+    python_requires=">=3.10",  # Minimum Python version
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
