aeac552e0117fb565df695045036bc0a3d752c50
Daniel <hora.daniel@protonmail.com>
2026-01-03 00:12:03 +0100

    Delete NumScript/support directory

--- NumScript/support/mostCommon.py
+++
@@ -1,9 +0,0 @@
-# --- Importing Libraries ---
-from collections import Counter
-
-# --- Checks for the most common item ---
-def mostCommon(self, lst):
-    if not lst: 
-        return None
-    
-    return Counter(lst).most_common(1)[0][0]
