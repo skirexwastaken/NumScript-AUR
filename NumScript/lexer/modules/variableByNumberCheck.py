443864fbc08a8079ebb598b7220a07b638478363
Daniel <hora.daniel@protonmail.com>
2026-01-03 00:11:54 +0100

    Delete NumScript/lexer directory

--- NumScript/lexer/modules/variableByNumberCheck.py
+++
@@ -1,8 +0,0 @@
-# --- Cleans var constructor, used in lexer ---
-def variableByNumberCheck(self):
-    if self.variableByNumber not in self.variables:
-        self.variables[self.variableByNumber] = "00"
-
-    self.lexerOutputPart += self.variables[self.variableByNumber]
-
-    self.variableByNumber = ""
